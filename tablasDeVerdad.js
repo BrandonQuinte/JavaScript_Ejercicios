0dd35ef135ceabf798460ab1aa0f385969f5d020
BrandonQuinte <brandonquintero028@gmail.com>
2025-04-02 21:03:15 -0500

    Subiendo ejercicio de tabla de verdad en JavaScript

---
+++ tablasDeVerdad.js
@@ -0,0 +1,8 @@
+console.log("| A     | B     | A && B |");
+console.log("|-------|-------|--------|");
+
+console.log("|" , (false != true) , " |" , (true || true) , " |" , (true || true) , "  |"); 
+console.log("|" , (true == true) , " |" , (true != true) , "|" , (true  && false) , " |"); 
+console.log("|" , (true != true) , "|" , (false == false) , " |" , (false && true)  , " |"); 
+console.log("|" , (true == false) , "|" , (true != true) , "|" , (false == false) , "  |");
+console.log("|-------|-------|--------|");
