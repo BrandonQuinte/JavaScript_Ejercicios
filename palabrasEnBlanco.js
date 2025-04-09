3bfaa4bbc50f86881f5ce831d46822be60352b48
BrandonQuintero <brandonquintero028@gmail.com>
2025-04-08 20:39:43 -0500

    Delete palabrasEnBlanco.js

--- palabrasEnBlanco.js
+++
@@ -1,7 +0,0 @@
-var miSustantivo = "perro"
-var miAdjetivo = "negro"
-var miVerbo = "corrió"
-var miAdverbio = "rápidamente"
-
-var palabrasEnBlanco = "El" + " " + miSustantivo + " " + miAdjetivo + " " + "descansó bajo un arbol. \nLuego," + miVerbo + " " + miAdverbio + " " + "hacia su casa"
-console.log(palabrasEnBlanco)
