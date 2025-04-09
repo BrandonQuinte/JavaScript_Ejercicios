a5c6e54a14295a52d0e8229888b680bd95bd9647
BrandonQuinte <brandonquintero028@gmail.com>
2025-04-08 20:29:42 -0500

    Subiendo tablas de verdad 2.0

---
+++ tablasDeVerdad2.0.js
@@ -0,0 +1,65 @@
+// Comprobando la tabla de verdad del operador lógico AND (&&)
+console.log("Tabla de verdad del operador && (AND)");
+console.log("| X | Y | X && Y |");
+console.log("------------------");
+console.log("| T | T |   T    |");
+console.log("------------------");
+console.log("| T | F |   F    |");
+console.log("------------------");
+console.log("| F | T |   F    |");
+console.log("------------------");
+console.log("| F | F |   F    |");
+
+// Mostrando la tabla de verdad para el operador OR (||)
+console.log("\nTabla de verdad del operador || (OR)");
+console.log("| X | Y | X || Y |");
+console.log("------------------");
+console.log("| T | T |   T    |");
+console.log("------------------");
+console.log("| T | F |   T    |");
+console.log("------------------");
+console.log("| F | T |   T    |");
+console.log("------------------");
+console.log("| F | F |   F    |");
+
+// Otra vez: tabla del operador AND para reforzar el concepto
+console.log("Tabla de verdad del operador && (AND)");
+console.log("| X | Y | X && Y |");
+console.log("------------------");
+console.log("| T | T |   T    |");
+console.log("------------------");
+console.log("| T | F |   F    |");
+console.log("------------------");
+console.log("| F | T |   F    |");
+console.log("------------------");
+console.log("| F | F |   F    |");
+
+// Segunda presentación de la tabla OR
+console.log("\nTabla de verdad del operador || (OR)");
+console.log("| X | Y | X || Y |");
+console.log("------------------");
+console.log("| T | T |   T    |");
+console.log("------------------");
+console.log("| T | F |   T    |");
+console.log("------------------");
+console.log("| F | T |   T    |");
+console.log("------------------");
+console.log("| F | F |   F    |");
+
+// Tabla que muestra cómo funciona el operador NOT (!)
+console.log("\nTabla de verdad del operador ! (NOT)");
+console.log("| X | !X |");
+console.log("----------");
+console.log("| T |  F |");
+console.log("----------");
+console.log("| F |  T |");
+
+// Comparación de valores booleanos usando condicional
+let X = true; 
+let Y = false;
+
+if (X == Y) {
+    console.log("X es igual a Y");
+} else {
+    console.log("X no es igual a Y");
+}
